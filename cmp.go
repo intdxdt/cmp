2059bc18e91f043aff6c6b066a9a089d2c78b5b3
Titus Tienaah <titus.intdxdt@gmail.com>
2017-11-02 07:58:20 -0300

    str

--- cmp.go
+++ cmp.go
@@ -5,21 +5,32 @@ import "github.com/intdxdt/math"
 type Compare func(a, b interface{}) int
 
 func Int(a, b interface{}) int {
-	d := a.(int)- b.(int)
-	if d > 0 {
-		return 1
-	} else if d < 0 {
-		return -1
-	}
-	return 0 //i - *v
+    d := a.(int) - b.(int)
+    if d > 0 {
+        return 1
+    } else if d < 0 {
+        return -1
+    }
+    return 0
+}
+
+func Str(a, b interface{}) int {
+    var as = a.(string)
+    var bs = b.(string)
+    if as > bs {
+        return 1
+    } else if as < bs {
+        return -1
+    }
+    return 0
 }
 
 func F64(a, b interface{}) int {
-	d := a.(float64) -  b.(float64)
-	if math.FloatEqual(d, 0) {
-		return 0
-	} else if d < 0 {
-		return -1
-	}
-	return 1
+    d := a.(float64) - b.(float64)
+    if math.FloatEqual(d, 0) {
+        return 0
+    } else if d < 0 {
+        return -1
+    }
+    return 1
 }
